832296339164711c52c5b5d2d3237cfb081943f6
Victor <vmendessforne@gmail.com>
2025-01-24 22:27:43 -0300

    Adicionando validações

--- WebMicroondas/Controllers/MicroondasController.cs
+++ WebMicroondas/Controllers/MicroondasController.cs
@@ -13,6 +13,7 @@ namespace WebMicroondas.Controllers
         {
             return View();
         }
+
         
         [HttpPost]
         public ActionResult IniciarAquecimento(Microondas microondas)
@@ -20,8 +21,17 @@ namespace WebMicroondas.Controllers
             var tempo = microondas.Tempo;
             var potencia = microondas.Potencia;
 
+            if (tempo < 1 || tempo > 120)
+                ModelState.AddModelError("", "O Tempo deve estar entre 1 e 120 segundos");
+            if (potencia < 1 || potencia > 10)
+                ModelState.AddModelError("", "A Potência deve estar entre 1 e 10");
+
+            if (!ModelState.IsValid)       
+                return View("Index", microondas);
+
             var resultado = tempo + potencia;
             ViewBag.Resultado = resultado;
+            ViewBag.PotenciaPadrao = potencia;
 
             return View("Index");
         }
