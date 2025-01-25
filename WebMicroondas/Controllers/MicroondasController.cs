318b8db52fc65ab4c0617a2b14c966cad5f46503
Victor <vmendessforne@gmail.com>
2025-01-24 21:31:20 -0300

    Iniciado o Projeto juntamente com o Front-end. Trabalhando na pasta controllers

---
+++ WebMicroondas/Controllers/MicroondasController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebMicroondas.Models;
+
+namespace WebMicroondas.Controllers
+{
+    public class MicroondasController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        
+        [HttpPost]
+        public ActionResult IniciarAquecimento(Microondas microondas)
+        {
+            var tempo = microondas.Tempo;
+            var potencia = microondas.Potencia;
+
+            var resultado = tempo + potencia;
+            ViewBag.Resultado = resultado;
+
+            return View("Index");
+        }
+
+    }
+}
---
+++ WebMicroondas/Models/Microondas.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebMicroondas.Models
+{
+    public class Microondas
+    {
+        public int Tempo { get; set; }
+        public int Potencia { get; set; }
+    }
+}
